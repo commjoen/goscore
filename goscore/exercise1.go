07275cab560a1ca661ff7b90c7c70e47559906ce
Jeroen Willemsen <jwillemsen@xebia.com>
2021-10-13 23:32:33 +0200

    updated first exercise

--- goscore/exercise1.go
+++ goscore/exercise1.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"html"
 	"log"
 	"net/http"
 	"strings"
@@ -19,6 +20,24 @@ func (s scoreServer) handleLeftExercise1() http.HandlerFunc {
 			return
 		}
 		value := strings.ToLower(r.FormValue("formValue"))
+		s.logger.Printf("we got response for exercise value 1, value: " + value)
+		w.Write([]byte(value))
+	}
+}
+
+func (s scoreServer) handleRightExercise1() http.HandlerFunc {
+	s.logger.Printf("Parsing exercise1 left form here")
+	return func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodPost {
+			http.Error(w, "404: only posts live here", http.StatusNotFound)
+			return
+		}
+		if err := r.ParseForm(); err != nil {
+			log.Fatal(w, "ParseForm() err: %v", err)
+			http.Error(w, "Error parsing form", http.StatusInternalServerError)
+			return
+		}
+		value := html.EscapeString(strings.ToLower(r.FormValue("formValue")))
 		s.logger.Printf("we got response for exercise value 1, value: " + value)
 		w.Write([]byte(value))
 	}
